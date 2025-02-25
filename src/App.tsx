6fbde0d823de4defbc735c29923ecad62b94f19f
swaminaing <smnaing9.smn@gmail.com>
2025-02-25 09:39:19 +0630

    first api method: GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "./utils/http";
+import Post from "./components/Post";
+import BlogPosts from "./components/BlogPosts";
+
 export type Post = {
   id: number;
   title: string;
@@ -5,7 +10,27 @@ export type Post = {
 };
 
 function App() {
-  return <main></main>;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    try {
+      async function handleGetPosts() {
+        const data = await getPosts();
+
+        setPosts(data);
+      }
+
+      handleGetPosts();
+    } catch {
+      throw new Error("Failed to fetch posts");
+    }
+  }, []);
+
+  return (
+    <main>
+      <BlogPosts posts={posts} />
+    </main>
+  );
 }
 
 export default App;
